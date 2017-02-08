63710d1e63f4462b434dd13a9ffc2ef88b07e5ae
Sylvain Marchienne <Sylvain@MacBook-Pro-de-Sylvain.local>
2017-02-08 18:03:39 +0100

    Add new data and new scripts

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
 from craftai import client as craftai
 from env import *
-import pandas as pd
 import json
-
-DATA_PATH = "./data/"
 
 ## 1) Connect to craftai api
 config = {
     "owner": CRAFT_OWNER,
     "token": CRAFT_TOKEN,
+    "url" : CRAFT_URL,
 }
 client = craftai.CraftAIClient(config)
 
 ## 2) Create the agent
-agent_id = "Paces"
+agent_id = "Wellness"
 configuration = {
     "context": {
-        "paces": {
-            "type": 'continuous'
+        "day_off": {                        # day is off or not
+            "type": "enum"
         },
-        "distance": {
-            "type": 'continuous'
+        "next_day_off": {                   # next day is off or not
+            "type": "enum"
         },
-        "deepsleep": {
-            "type": 'continuous'
+        "sleep_at_home": {                  # sleep at home or not
+            "type" : "enum"
+        },
+        "sleep_start": {                    # hour at which he started to sleep
+            "type": "time_of_day",
+            "is_generated" : False,
+        },
+        "timezone": {                       # timezone for sleep_start hour
+            "type" : "timezone"
+        },
+        "sleep": {                          # sleep time during the night
+            "type": "continuous"
         }
     },
-    "output": ['deepsleep']
+    "output": ["sleep"],                    # the output is continuous
 }
 
-# Delet older version of the agent
+# Delete older version of the agent
 client.delete_agent(agent_id)
 print("Agent", agent_id, "no longer exists")
 
@@ -39,17 +47,23 @@ print("Agent", agent["id"], "has successfully been created")
 
 ## 3) Load and send the historical data
 context_list = []
-with open(DATA_PATH + 'clean_data.json') as data_file:
+with open("data/clean_data.json") as data_file:
     context_list = json.load(data_file)
 
 client.add_operations(agent_id, context_list)
 print("List of contexts added")
 
 ## 4) Retreive the decison tree
-dt_timestamp = 1477000800 # computed tree from the 2016-10-21 (example)
+dt_timestamp = 1484521200 # computed tree at the 2017-01-16 (example)
 decision_tree = client.get_decision_tree(agent_id, dt_timestamp)
 
 ## 5) Compute a decision based on a context
-context = { "paces" : 10000, "distance" : 2156 }
+context = {
+    "day_off" : "False",             # worked during the day
+    "next_day_off" : "True",         # doesn't work the next day
+    "sleep_at_home" : "True",        # sleep at home
+    "sleep_start" : 23.5,            # start to sleep at 23:30:00
+    "timezone" : "+02:00"            # timezone for sleep_start
+}
 resp = client.decide(decision_tree, context)
-print("The anticipated deep sleep time is:", resp["decision"]["deepsleep"], "seconds")
+print("The anticipated sleep time is:", resp["decision"]["sleep"], "hours")
--- prepare.py
+++ prepare.py
@@ -1,55 +1,34 @@
 import pandas as pd
+from datetime import datetime
 import json
-import time
-import datetime
 
-DATA_PATH = "./data/"
+# Load the dataset
+data = pd.read_csv('data/sleep_data.csv')
 
-def get_date(row):
-    start = datetime.datetime.strptime(row["Début"], "%Y-%m-%d %H:%M")
-    # if he starts sleeping after midnight, the active day is just the one before
-    if (start.hour >= 0 and start.hour <= 12):
-        start = start - datetime.timedelta(days=1)
-    return start.strftime('%Y-%m-%d')
+# Convert date into timestamp
+data["timestamp"] = data.apply(lambda x: datetime.strptime(x['date'], '%Y-%m-%d').timestamp(), axis=1).astype('int64')
 
-def to_timestamp(row):
-    return time.mktime(datetime.datetime.strptime(row["Date"], "%Y-%m-%d").timetuple())
+# Enum type must be string
+data["day_off"] = data["day_off"].astype(str)
+data["next_day_off"] = data["next_day_off"].astype(str)
+data["sleep_at_home"] = data["sleep_at_home"].astype(str)
 
-# First dataset: activities.csv
-data1 = pd.read_csv(DATA_PATH + "activities.csv")
-data1 = data1.drop(["Dénivelé (m)", "Calories actives"], axis = 1)
-
-# Second dataset: sleep.csv
-data2 = pd.read_csv(DATA_PATH + "sleep.csv")
-data2["Date"] = data2.apply(get_date, axis = 1)
-data2 = data2.drop_duplicates(['Date'], keep='last') # drop naps
-data2 = data2[["réveils", "Date", "profond (s)"]]
-
-# Merge on date
-data = pd.merge(data1, data2, how='inner', on='Date')
-data["timestamp"] = data.apply(to_timestamp, axis = 1)
-data = data.drop(["Date"], axis = 1)
-data.columns = ["paces", "distance", "wakeups", "deepsleep", "timestamp"]
-
-# Casting
-data["timestamp"] = data["timestamp"].astype("int64")
-data["paces"] = data["paces"].astype("int64")
-data["distance"] = data["distance"].str.replace(',', '.')
-data["distance"] = data["distance"].astype("float64")
-
-# Craftai structure
+# Convert in craft ai python dictionnary structure
 context_list = []
 for index, row in data.iterrows():
     context_list.append({
         "timestamp" : row["timestamp"],
         "diff" : {
-            "paces" : row["paces"],                 # number of pace during the day
-            "distance" : row["distance"],           # travaled distance during the day
-            "deepsleep" : row["deepsleep"],         # deep sleep time during the night
+            "day_off" : row["day_off"],
+            "next_day_off" : row["next_day_off"],
+            "sleep_at_home" : row["sleep_at_home"],
+            "sleep_start" : row["sleep_start"],
+            "sleep" : row["sleep"],
+            "timezone" : row["timezone"]
         }
     })
 
 # Output
-with open(DATA_PATH + 'clean_data.json', 'w') as outfile:
-    json.dump(context_list, outfile, indent = 4)
-print("Saved to " + DATA_PATH + "clean_data.json")
+with open('data/clean_data.json', 'w') as outfile:
+    json.dump(context_list, outfile, indent = 3)
+print("Saved to data/clean_data.json")
