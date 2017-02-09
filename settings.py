c8a2be7b562fab4733e1cc99e8509cf4175e7d0c
Sylvain Marchienne <sylvain.marchienne@gmail.com>
2017-02-09 17:17:10 +0100

    env file + requirements + src dir

--- settings.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv())
-CRAFT_CFG = {
-    "owner": os.environ.get("CRAFT_OWNER"),
-    "token": os.environ.get("CRAFT_TOKEN"),
-    "url": os.environ.get("CRAFT_URL")
-}
