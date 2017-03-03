c017342bad37c8e1cd0e21ae01fcc4cd07f0efb5
Perrine Brunel <perrine.brunel@gmail.com>
2017-03-03 10:36:34 +0100

    Update the load of the configuration with project

--- src/settings.py
+++ src/settings.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 CRAFT_CFG = {
     "owner": os.environ.get("CRAFT_OWNER"),
+    "project": os.environ.get("CRAFT_PROJECT"),
     "token": os.environ.get("CRAFT_TOKEN"),
     "url": os.environ.get("CRAFT_URL")
 }
